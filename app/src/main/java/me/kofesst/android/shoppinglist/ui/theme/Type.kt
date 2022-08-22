15c4055c7eedfc18b9757143927fc28c9fae6b2f
kofesst <xkof3st.mail@gmail.com>
2022-08-22 18:48:06 +0300

    Изменен шрифт в приложении

--- app/src/main/java/me/kofesst/android/shoppinglist/ui/theme/Type.kt
+++ app/src/main/java/me/kofesst/android/shoppinglist/ui/theme/Type.kt
@@ -1,34 +1,36 @@
 package me.kofesst.android.shoppinglist.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import me.kofesst.android.shoppinglist.R
 
-// Set of Material typography styles to start with
+val appFontFamily = FontFamily(
+    // Normal
+    Font(R.font.exo2_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.exo2_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.exo2_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.exo2_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.exo2_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.exo2_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.exo2_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.exo2_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.exo2_black, FontWeight.Black, FontStyle.Normal),
+
+    // Italic
+    Font(R.font.exo2_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.exo2_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.exo2_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.exo2_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.exo2_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.exo2_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.exo2_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.exo2_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.exo2_black_italic, FontWeight.Black, FontStyle.Italic)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    defaultFontFamily = appFontFamily
 )
