7d616ad0321cfa9c96762a29e26cdcf8ccbf9ae4
kofesst <xkof3st.mail@gmail.com>
2022-08-22 13:34:56 +0300

    Весь текст в приложении разделен на категории

--- app/src/main/java/me/kofesst/android/shoppinglist/presentation/utils/FormUtils.kt
+++ app/src/main/java/me/kofesst/android/shoppinglist/presentation/utils/FormUtils.kt
@@ -5,23 +5,23 @@ import me.kofesst.android.shoppinglist.domain.utils.AuthResult
 
 val ValidationResult.errorMessage: UiText?
     get() = when (this) {
-        ValidationResult.NullField -> emptyFieldError
-        ValidationResult.EmptyField -> emptyFieldError
-        ValidationResult.InvalidInteger -> invalidIntError
-        ValidationResult.NotInRange -> notInRangeError
-        ValidationResult.LongLength -> longLengthError
-        ValidationResult.SmallLength -> smallLengthError
-        ValidationResult.InvalidEmail -> invalidEmailError
-        ValidationResult.PasswordContainsSpace -> passwordContainsSpaceError
-        ValidationResult.PasswordNeedDigit -> passwordNeedDigitError
-        ValidationResult.PasswordNeedLetters -> passwordNeedLetterError
+        ValidationResult.NullField -> AppText.Error.emptyFieldError
+        ValidationResult.EmptyField -> AppText.Error.emptyFieldError
+        ValidationResult.InvalidInteger -> AppText.Error.invalidIntError
+        ValidationResult.NotInRange -> AppText.Error.notInRangeError
+        ValidationResult.LongLength -> AppText.Error.longLengthError
+        ValidationResult.SmallLength -> AppText.Error.smallLengthError
+        ValidationResult.InvalidEmail -> AppText.Error.invalidEmailError
+        ValidationResult.PasswordContainsSpace -> AppText.Error.passwordContainsSpaceError
+        ValidationResult.PasswordNeedDigit -> AppText.Error.passwordNeedDigitError
+        ValidationResult.PasswordNeedLetters -> AppText.Error.passwordNeedLetterError
         ValidationResult.Success -> null
     }
 
 val AuthResult.Failed.errorMessage: UiText
     get() = when (this) {
-        AuthResult.Failed.EmailAlreadyExists -> emailAlreadyExistsError
-        AuthResult.Failed.IncorrectPassword -> incorrectPasswordError
-        AuthResult.Failed.InvalidUser -> invalidUserError
+        AuthResult.Failed.EmailAlreadyExists -> AppText.Error.emailAlreadyExistsError
+        AuthResult.Failed.IncorrectPassword -> AppText.Error.incorrectPasswordError
+        AuthResult.Failed.InvalidUser -> AppText.Error.invalidUserError
         is AuthResult.Failed.Unexpected -> UiText.Static(exception.toString())
     }
