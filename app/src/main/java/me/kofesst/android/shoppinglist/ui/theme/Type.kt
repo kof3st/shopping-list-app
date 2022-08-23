e78e4dc3a10d04cc7c5cf786f07ad3ff5d99571e
kofesst <xkof3st.mail@gmail.com>
2022-08-23 10:35:34 +0300

    Изменен шрифт приложения

--- app/src/main/java/me/kofesst/android/shoppinglist/ui/theme/Type.kt
+++ app/src/main/java/me/kofesst/android/shoppinglist/ui/theme/Type.kt
@@ -9,7 +9,29 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import me.kofesst.android.shoppinglist.R
 
-val AppFontFamily = FontFamily.Default
+val AppFontFamily = FontFamily(
+    // Normal
+    Font(R.font.exo2_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.exo2_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.exo2_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.exo2_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.exo2_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.exo2_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.exo2_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.exo2_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.exo2_black, FontWeight.Black, FontStyle.Normal),
+
+    // Italic
+    Font(R.font.exo2_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.exo2_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.exo2_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.exo2_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.exo2_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.exo2_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.exo2_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.exo2_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.exo2_black_italic, FontWeight.Black, FontStyle.Italic)
+)
 
 val AppTypography = Typography(
     labelLarge = TextStyle(
